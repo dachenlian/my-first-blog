c14f3510c359552439ab7bcae1b6193dd0c0d785
dachenlian <cclemon.lian@gmail.com>
2018-03-05 15:38:09 +0800

    .gitignore is now working

--- venv/Scripts/conda.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call "C:\Users\richl\Anaconda3\Scripts\conda" %*
--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\richl\Google Drive\Programming\PycharmProjects\djangogirls\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
